640cc68c0e71ee62d31778daa6753eaba925a6d5
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-22 21:29:00 +0300

    chore -podcasts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,15 +9,17 @@ const Home = () => {
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
         <Button className="text-white-1">Trending Podcasts</Button>
-        {podcastData.map((podcast) => (
-          <PodcastCard
-            key={podcast.id}
-            imgUrl={podcast.imgURL}
-            title={podcast.title}
-            description={podcast.description}
-            podcastId={podcast.id}
-          />
-        ))}
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => (
+            <PodcastCard
+              key={podcast.id}
+              imgUrl={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
