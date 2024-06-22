1c078500cc88c7920902727fb6d386d148c2cf07
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-22 17:30:00 +0300

    chore - postcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import { PodcastCard } from "@/components/PodcastCard";
 import { Button } from "@/components/ui/button";
+import { podcastData } from "@/constants";
 import React from "react";
 
 const Home = () => {
@@ -7,6 +9,15 @@ const Home = () => {
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
         <Button className="text-white-1">Trending Podcasts</Button>
+        {podcastData.map((podcast) => (
+          <PodcastCard
+            key={podcast.id}
+            imgUrl={podcast.imgURL}
+            title={podcast.title}
+            description={podcast.description}
+            podcastId={podcast.id}
+          />
+        ))}
       </section>
     </div>
   );
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const PodcastCard = ({
+  imgUrl,
+  title,
+  description,
+  podcastId,
+}: {
+  imgUrl: string;
+  title: string;
+  description: string;
+  podcastId: number;
+}) => {
+  return <div className="cursor-pointer"></div>;
+};
